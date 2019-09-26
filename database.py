6808dbd351c5c45cd86cad2b53257cda5a0dc9e8
Luis <luis@luisem.co>
2019-09-25 23:39:07 -0300

    Unknow field

--- database.py
+++ database.py
@@ -5,10 +5,16 @@ import datetime
 database = peewee.MySQLDatabase('stock_market', host='localhost', port=3306,user='root', password='root')
 
 class Symbol(peewee.Model):
-	symbol_id = peewee.BigIntegerField(primary_key=True)
+	symbol_id = peewee.AutoField()
 	stock_symbol = peewee.CharField()
-	stock_exchange
+	stock_exchange = peewee.CharField()
+	unknown_field = peewee.CharField()
+	currency = peewee.CharField()
+	routing = peewee.CharField()
 	created_at = peewee.DateTimeField(default=datetime.datetime.now)
 	class Meta:
 		database = database
 		db_table = 'symbol'
+
+if __name__ == '__main__':
+	Symbol.create_table()
