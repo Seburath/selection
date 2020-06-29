ccbe1b0a397c1de9dcc31d878a7a036e5b8a2043
Geoffery Joseph <geofferyjoseph1@gmail.com>
2020-06-29 14:58:20 +0100

    new data repo

--- backtester.py
+++ backtester.py
@@ -1,7 +1,11 @@
 from backtesting import Backtest, Strategy
 from backtesting.lib import crossover
 
-from backtesting.test import SMA, GOOG
+from backtesting.test import SMA, 
+import quandl
+
+quandl.ApiConfig.api_key= "1JzcSRocvMqEaHSrEyyB"
+data = quandl.get("WIKI/GOOGL")
 
 
 class SmaCross(Strategy):
@@ -11,6 +15,9 @@ class SmaCross(Strategy):
     def init(self):
         self.sma1 = self.I(SMA, self.data.Close, self.n1)
         self.sma2 = self.I(SMA, self.data.Close, self.n2)
+        data = bt.feeds.YahooFinanceData(dataname='AAPL',
+                                 fromdate=datetime(2016, 1, 1),
+                                 todate=datetime(2019, 5, 31))
 
     def next(self):
         if crossover(self.sma1, self.sma2):
@@ -19,7 +26,7 @@ class SmaCross(Strategy):
             self.sell()
 
 
-bt = Backtest(GOOG, SmaCross, cash=10000, commission=.002)
+bt = Backtest(data, SmaCross, cash=10000, commission=.002)
 
 output = bt.run()
 bt.plot()
